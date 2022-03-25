b668a52bdd136f19e5070c279e67d30b90529848
mashraza881 <101043312+mashraza881@users.noreply.github.com>
2022-03-25 15:45:24 +0530

    Add files via upload

---
+++ Block.js
@@ -0,0 +1,24 @@
+class Block{
+    constructor(x, y, width, height) {
+        var options = {
+            restitution :0.4,
+            friction :0.0,
+            isStatic : false
+        }
+        
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.width = width;
+        this.height = height;
+         this.image= loadImage("block.png")
+        World.add(world, this.body);
+      }
+
+      display(){
+
+        var pos= this.body.position;
+        imageMode(CENTER);
+        image(this.image, this.body.position.x, this.body.position.y,this.width, this.height);
+
+
+        }
+}
